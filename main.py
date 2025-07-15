10f850b8b8290a76797c69fa433f7f06f5b26690
Arnaud Thery <St4r4x@gmail.com>
2025-07-15 14:48:03 +0200

    Fix typo in variable name for contours in edge detection process

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ try:
         gray = cv2.bilateralFilter(gray, 11, 17, 17)
 
         edged = cv2.Canny(gray, 30, 200)  # Perform Edge detection
-        nts = cv2.findContours(
+        cnts = cv2.findContours(
             edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
         cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:10]
